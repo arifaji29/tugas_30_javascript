ddf53b0106423c25244deececf3b6a96ecd307d3
Arif <arifajif@gmail.com>
2022-06-02 07:19:02 +0700

    tugas 30

---
+++ tugas30.js
@@ -0,0 +1,9 @@
+function regex(value) {
+    
+   var ambilData = value.match(/[es]/g);
+  
+    console.log(ambilData);
+   }
+   
+   regex("Para tetua adat mendiskusikan masalah kejahatan yang terjadi di rumah adat");
+
